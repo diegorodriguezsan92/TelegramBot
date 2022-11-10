142475b0160a1f08d6f10b3464e7d26d49f5a046
dugalatortuga <gab2diegor@gmail.com>
2022-11-10 08:58:20 +0100

    Re-order

--- Program.cs
+++ Program.cs
@@ -21,30 +21,31 @@ namespace TelegramBot
         async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             var message = update.Message;
-            var chatId = message.Chat.Id;
 
-
-            Console.WriteLine($"Start listening for @{message.Chat.FirstName}");
-
-            Console.WriteLine($"{message.Chat.FirstName}   |   {message.Text}");
-            await botClient.SendTextMessageAsync(message.Chat.Id, $"Welcome to Duga, la tortuga bot, {message.Chat.FirstName}.");
-
+            if (message.Text != null)
+            {
+                Console.WriteLine($"{message.Chat.FirstName}  |   {message.Text}");
+                if (message.Text.ToLower().Contains("Hi"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Bienvenido al bot de Duga, la tortuga!");
+                    return;
+                }
+            }
 
             if (message.Photo != null)
             {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Your image has been uploaded successfully.");
+                await botClient.SendTextMessageAsync(message.Chat.Id, "La imagen se ha cargado correctamente.");
                 return;
             }
 
             if (message.Document != null)
             {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "The document has been uploaded successfully.");
+                await botClient.SendTextMessageAsync(message.Chat.Id, "El documento se ha cargado correctamente.");
 
+                // Downloading files
                 var fileId = update.Message.Document.FileId;
                 var fileInfo = await botClient.GetFileAsync(fileId);
                 var filePath = fileInfo.FilePath;
-
-                // Console.WriteLine(fileId, fileInfo, filePath);
 
                 string destinationFilePath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\{message.Document.FileName}";
                 await using FileStream fileStream = System.IO.File.OpenWrite(destinationFilePath);
@@ -52,23 +53,17 @@ namespace TelegramBot
                 fileStream.Close();
 
                 Process.Start(@"C:\Users\Diego\Desktop\prueba.exe", $@"""{destinationFilePath}""");
-                await Task.Delay(1500);
+                await Task.Delay(2000);
 
+                // Uploading files
                 await using Stream stream = System.IO.File.OpenRead(destinationFilePath);
-                await botClient.SendDocumentAsync(message.Chat.Id, new InputOnlineFile(stream, message.Document.FileName.Replace(".jpg", ".jpeg")));
+                await botClient.SendDocumentAsync(message.Chat.Id, new InputOnlineFile(stream, message.Document.FileName.Replace(".jpg", "(edited).jpg")));
 
                 return;
             }
-            if (message.Text.ToLower().Contains("Contact"))    // TO DO: complete this snippet
-            {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Please, send me a random contact from your phone and I will send you another one from my agenda.");
-                if (message.Contact != null)
-                {
-                    await botClient.SendContactAsync(message.Chat.Id, phoneNumber: "+1234567890", firstName: "Darth", lastName: "Vader");
-                }
-            }
 
-            if (message.Text != null) // TO DO: verify this snippet exits properly
+            // Answers with buttons
+            if (message.Text.ToLower().Contains("Age"))
             {
 
                 ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
@@ -76,24 +71,23 @@ namespace TelegramBot
                     new KeyboardButton[] { "<18", "19-30" },
                     new KeyboardButton[] { "31-50", "51-70" },
                     new KeyboardButton[] { ">70", "NC" }
-
                 })
                 {
                     ResizeKeyboard = true
                 };
 
                 Message sentMessage1 = await botClient.SendTextMessageAsync(
-                    chatId: chatId,
+                    chatId: message.Chat.Id,
                     text: "How old are you?",
                     replyMarkup: replyKeyboardMarkup,
                     cancellationToken: cancellationToken);
 
-                await Task.Delay(5000); // TO DO: wait until user's answer.
+                await Task.Delay(5000); // TODO: wait until user's answer.
                 await botClient.SendTextMessageAsync(message.Chat.Id, "Amazing!");
                 return;
             }
-        }
 
+        }
         private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
         {
             throw new NotImplementedException();
